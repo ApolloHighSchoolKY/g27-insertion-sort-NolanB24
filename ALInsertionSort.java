36340544fb6998fc803f37c856d1f6b282fb3163
NolanB24 <nolan.bretz@stu.daviess.kyschools.us>
2026-03-11 22:37:29 +0000

    commit

--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class ALInsertionSort
 {
 
@@ -8,15 +8,27 @@ public class ALInsertionSort
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
     	ArrayList<Integer> sorted = new ArrayList<Integer>();
     	boolean inserted=false;
-
+		int passes = myNumbers.length;
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
-
+		for(int pass = 1; pass < passes; pass++)
+		{
 			//Check with each of the numbers in the sorted list
+			for(int y = 0; y < sorted.size(); y++)
+			{
+				if(myNumbers[pass] < sorted.get(y))
+				{
+					sorted.add(myNumbers[pass],y);
+				}
 
+					
+			}
+				sorted.add(myNumbers[pass]);
 				//If this number is less than one in the sorted list,
 				//insert it there
+		}
+			
 
 			//If it was not inserted, stick it on the end.
 
@@ -26,9 +38,10 @@ public class ALInsertionSort
 		System.out.println(sorted);
 
 		//Move the data back to the array
-
+		for(int x = 0; x < sorted.size(); x++)
+			myNumbers[x] = sorted.get(x);
 		//Print the contents of the array
-
+		System.out.println(Arrays.toString(myNumbers));
     }
 
 
